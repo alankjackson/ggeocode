1251d398a1e552c59cd2cfe4fe7bffe22fdb952f
alankjackson <alan@ajackson.org>
2020-03-03 21:32:16 -0600

    Change opencage test to data.frame

--- tests/testthat/test-opencage_basic.R
+++ tests/testthat/test-opencage_basic.R
@@ -3,5 +3,5 @@ context("opencage_basic")
 
 test_that("opencage returns a list",{
   expect_is(opencage(placename="100 Main, Houston, Tx, 77008, USA",
-                     keyfile="/home/ajackson/AccessTokens.txt"), "list")
+                     keyfile="/home/ajackson/AccessTokens.txt"), "data.frame")
 })
