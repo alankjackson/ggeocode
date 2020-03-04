950a47470597538942748843e0c45aaad51cecb0
alankjackson <alan@ajackson.org>
2020-03-04 15:38:43 -0600

    Successful added test for compare_addys

--- tests/testthat/test-opencage_basic.R
+++ tests/testthat/test-opencage_basic.R
@@ -1,7 +1,13 @@
 library("ggeocode")
 context("opencage_basic")
 
-test_that("opencage returns a list",{
+test_that("opencage returns a data frame",{
   expect_is(opencage(placename="100 Main, Houston, Tx, 77008, USA",
                      keyfile="/home/ajackson/AccessTokens.txt"), "data.frame")
+})
+
+test_that("compare_addys returns a vector",{
+  expect_equal(compare_addys(address_1="100 Main, Houston, Tx 77008, USA",
+                             address_2="101 Main, Houston, Tx"),
+            c(1,0,0,-1,-1))
 })
